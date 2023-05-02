3392fffec9b18db59be8c399fc5754c485dbd03f
Jupie <you@example.com>
2023-05-02 09:38:33 +0300

    Engine speed ECU skatch

---
+++ ECU/ServoEncoder/ServoEncoder.ino
@@ -0,0 +1,31 @@
+
+#include <Servo.h>
+
+
+Servo myservo;  // create servo object to control a servo
+
+int potpin = A1;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo
+					  // object
+  Serial.begin(115200);
+  delay(300); // .3 sec delay
+}
+
+
+void loop() {
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  Serial.print(val, DEC);
+  Serial.print(" ");
+
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  Serial.println(val, DEC);
+
+  myservo.write(val);                  // sets the servo position according to the scaled value
+
+  delay(15);                           // waits for the servo to get there
+
+}
